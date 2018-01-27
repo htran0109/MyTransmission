26a8dd8680100b1114038f60d921ac3efc6ff0dd
Abdul Hadi <damienhadios@gmail.com>
2018-01-27 21:41:54 +0800

    Add in Music.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioController.PlayMusicPlaylist ("MainTheme");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
