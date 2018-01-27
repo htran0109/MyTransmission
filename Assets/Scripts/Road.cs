6f32e801c29ea7be2d2de0d572c521f7bd117cc0
Abdul Hadi <damienhadios@gmail.com>
2018-01-27 17:28:32 +0800

    Move the road.

---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour {
+
+	[SerializeField] float speed = 0.1f/60;
+	public MeshRenderer roadTex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		roadTex.material.mainTextureOffset = new Vector2(0, roadTex.material.mainTextureOffset.y - speed);
+	}
+}
