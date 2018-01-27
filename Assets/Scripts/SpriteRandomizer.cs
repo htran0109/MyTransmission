b62ed07c49b1793ce5a6228f71e05f5e9005ecdd
Satrio Triatmoko <tio.triatmoko@outlook.com>
2018-01-28 01:38:52 +0800

    update the sprites for the other car

---
+++ Assets/Scripts/SpriteRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+	[SerializeField]
+	private Sprite[] sprites;
+
+	private SpriteRenderer sr; 
+	// Use this for initialization
+	void Start () {
+		this.sr = GetComponent<SpriteRenderer> ();
+
+		sr.sprite = sprites [Random.Range (0, this.sprites.Length)];
+	}
+
+}
