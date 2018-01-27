8d4fea2c692298f32c30290b09c24aaaf5d30018
hqtran <hqtran@ucsd.edu>
2018-01-27 14:45:13 +0800

    Car hits rocks and NPCars now

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+    Vector3 movementDirection;
+    CarParts playerCar;
+    // Use this for initialization
+    [SerializeField]
+    float movementSpeed_UnityUnitPerSecond;
+
+    void Start () {
+        movementDirection = transform.up * -1;
+        playerCar = GameObject.FindGameObjectWithTag("playerCar").GetComponent<CarParts>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += movementSpeed_UnityUnitPerSecond * movementDirection * Time.deltaTime;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("Trigger hit");
+        if(coll.gameObject.tag == "playerCar")//rock hit car
+        {
+            //do some damage step
+
+            Destroy(this.gameObject);
+            playerCar.damageCar();
+        }
+        if(coll.gameObject.tag == "skater") //rock hit skater
+        {
+            //do some stun step
+            Destroy(this.gameObject);
+        } 
+    }
+}
