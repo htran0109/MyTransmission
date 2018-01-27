d1360ecd25b950afc2a59d1b03dbca38f5fc1ab3
Abdul Hadi <damienhadios@gmail.com>
2018-01-27 20:54:29 +0800

    Add in tree spawner and tree.

---
+++ Assets/Scripts/TreeSpawning.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawning : MonoBehaviour {
+	[SerializeField] GameObject treePrefab;
+
+	[SerializeField] float minTimer;
+	[SerializeField] float maxTimer;
+	public float timer;
+
+	[SerializeField] float xStart;
+	[SerializeField] float yStart;
+
+	// Use this for initialization
+	void Start () {
+		timer = Random.Range (minTimer, maxTimer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			int multipler = 1;
+
+			if (Random.value > 0.5) {
+				multipler = -1;
+			}
+
+			// Spawn the tree!
+			Instantiate(treePrefab, new Vector3(multipler*Random.Range(xStart, xStart+10), yStart, 0), Quaternion.identity);
+			timer = Random.Range (minTimer, maxTimer);
+		}
+	}
+}
