5b1296ada2af69a6dc4aac45e83f677abc281cde
Satrio Triatmoko <tio.triatmoko@outlook.com>
2018-01-27 13:56:52 +0800

    Commit To Merge

---
+++ Assets/Scripts/PartsMovingBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsMovingBehavior : MonoBehaviour {
+	private Vector3 targetPosition; 	
+	private Vector3 originalPosition;
+	private Vector3 direction; 
+
+	[SerializeField]
+	private float flungSpeed;
+
+
+	private bool isMoving; 
+
+	void Awake() {
+		this.originalPosition = transform.position;
+		isMoving = false; 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isMoving) {
+			if (transform.position == targetPosition) {
+				Debug.Log ("target has been reacehed");
+				isMoving = false; 
+				return;
+			}
+			float step = flungSpeed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, targetPosition, step);
+		}
+	}
+
+	public void flungTo(Vector3 targetPosition){
+		this.targetPosition = targetPosition;
+		isMoving = true; 
+	}
+}
--- Assets/Scripts/nonPlayableCarMovement.cs
+++ Assets/Scripts/nonPlayableCarMovement.cs
@@ -10,7 +10,7 @@ public class nonPlayableCarMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		movementDirection = transform.up * -1; 
-	}
+	}	
 	
 	// Update is called once per frame
 	void Update () {
